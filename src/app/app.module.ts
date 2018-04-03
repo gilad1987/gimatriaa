ef8c06c8ad6889154121bb2315d05a15a7c87b8e
Gilad Takoni <gilad1987@gmail.com>
2018-04-03 23:23:35 +0300

    generate search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 
 import {GimatriaService} from './services/gimatria/gimatria.service';
-import {SearchComponent} from './components/search/search/search.component';
+import {SearchComponent} from './components/search/search.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
-        SearchComponent
+        SearchComponent,
     ],
     imports: [
         BrowserModule
